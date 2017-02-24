4aa239cfc3b253ade4e8858a12453f16116700a3
yaojie.hou <yaojie.hou@ilaw66.com>
2017-02-24 17:34:14 +0800

    generate gradle file and directory structure of spring boot

---
+++ src/main/java/com/marshall/FluffyPotatoApplication.java
@@ -0,0 +1,12 @@
+package com.marshall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluffyPotatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluffyPotatoApplication.class, args);
+	}
+}
