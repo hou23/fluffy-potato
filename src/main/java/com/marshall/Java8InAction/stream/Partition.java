47a055490b31d8ca355c2a69e77558bc068f48f0
yaojie.hou <yaojie.hou@ilaw66.com>
2017-03-17 18:47:37 +0800

    多级分区

--- src/main/java/com/marshall/Java8InAction/stream/Partition.java
+++ src/main/java/com/marshall/Java8InAction/stream/Partition.java
@@ -2,13 +2,14 @@ package com.marshall.Java8InAction.stream;
 
 import com.marshall.Java8InAction.domain.Dish;
 
+import javax.crypto.Cipher;
 import java.util.*;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 import static com.marshall.Java8InAction.domain.Dish.menu;
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.partitioningBy;
-import static java.util.stream.Collectors.toList;
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.*;
 
 /**
  * Created by yaojie.hou on 2017/3/16.
@@ -23,9 +24,37 @@ public class Partition {
         System.out.println(partitionedMenu);
 
         //partitioningBy方法也能传递第二个收集器
-        Map<Boolean, Map<Dish.Type, List<Dish>>> mostCaloricPartitionedByVegetarian =
+        Map<Boolean, Map<Dish.Type, List<Dish>>> vegetarianDishesByType =
                 menu.stream().collect(partitioningBy(Dish::isVegetarian, groupingBy(Dish::getType)));
         //{false={MEAT=[pork, beef, chicken], FISH=[prawns, salmon]}, true={OTHER=[french fries, rice, season fruit, pizza]}}
-        System.out.println(mostCaloricPartitionedByVegetarian);
-    }
+        System.out.println(vegetarianDishesByType);
+
+		Map<Boolean, Dish> mostCaloricPartitionedByVegetarian =
+				menu.stream().collect(partitioningBy(Dish::isVegetarian,
+						collectingAndThen(maxBy(comparing(Dish::getCalories)), Optional::get)));
+		//{false=pork, true=pizza}
+		System.out.println(mostCaloricPartitionedByVegetarian);
+
+		//多级分区
+		Map<Boolean, Map<Boolean, List<Dish>>> vegetarianAndCaloricDishes =
+				menu.stream().collect(partitioningBy(Dish::isVegetarian, partitioningBy(d -> d.getCalories() > 500)));
+		//{false={false=[chicken, prawns, salmon], true=[pork, beef]}, true={false=[rice, season fruit], true=[french fries, pizza]}}
+		System.out.println(vegetarianAndCaloricDishes);
+
+		Map<Boolean, Long> vegetarianDishCount =
+				menu.stream().collect(partitioningBy(Dish::isVegetarian, counting()));
+		//{false=5, true=4}
+		System.out.println(vegetarianDishCount);
+
+		//区分质数和非质数
+		Map<Boolean, List<Integer>> partitionPrimes =
+				IntStream.rangeClosed(2, 20).boxed().collect(partitioningBy(Partition::isPrime));
+		//{false=[4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20], true=[2, 3, 5, 7, 11, 13, 17, 19]}
+		System.out.println(partitionPrimes);
+	}
+
+	private static boolean isPrime(int candidate){
+    	int candidateRoot = (int) Math.sqrt(candidate);
+		return IntStream.rangeClosed(2, candidateRoot).noneMatch(i -> candidate % i == 0);
+	}
 }
--- src/main/java/com/marshall/Java8InAction/stream/SpecialStream.java
+++ src/main/java/com/marshall/Java8InAction/stream/SpecialStream.java
@@ -55,7 +55,10 @@ public class SpecialStream {
         pythagoreanTriples.limit(5).forEach(n -> System.out.println(n[0] + "," + n[1] + "," + n[2]));
 
         //勾股数-第二种优化方案,先生成所有三元数，再排除非整数
-        Stream<double[]> pythagoreanTriples2 = IntStream.rangeClosed(1, 100).boxed().flatMap(a -> IntStream.rangeClosed(a, 100).mapToObj(b -> new double[]{a, b, Math.sqrt(a * a + b * b)}).filter(m -> m[2] % 1 == 0));
+        Stream<double[]> pythagoreanTriples2 = IntStream
+				.rangeClosed(1, 100).boxed().flatMap(a ->
+						IntStream.rangeClosed(a, 100).mapToObj(b ->
+								new double[]{a, b, Math.sqrt(a * a + b * b)}).filter(m -> m[2] % 1 == 0));
         //3.0,4.0,5.0  5.0,12.0,13.0  6.0,8.0,10.0  7.0,24.0,25.0  8.0,15.0,17.0
         pythagoreanTriples2.limit(5).forEach(n -> System.out.println(n[0] + "," + n[1] + "," + n[2]));
 
