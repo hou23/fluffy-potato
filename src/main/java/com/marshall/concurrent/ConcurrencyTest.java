928ea76a9e95befe815f4d9f94cff9ecf3fe836e
yaojie hou <marshall.houz@gmail.com>
2017-05-14 23:53:47 +0800

    并发与串行性能比较, 死锁demo

---
+++ src/main/java/com/marshall/concurrent/ConcurrencyTest.java
@@ -0,0 +1,50 @@
+package com.marshall.concurrent;
+
+/**
+ * Created by marshall.houz on 2017/5/14.
+ * 当count为100w时, 并发执行速度才和串行一致.
+ * 在量小的时候, 并发执行并没有串行执行快. 因为线程有创建和上下文切换的开销.
+ */
+public class ConcurrencyTest {
+    private static final long count = 1000000;
+
+    public static void main(String[] args) throws InterruptedException {
+        concurrency();
+        serial();
+    }
+
+    private static void concurrency() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (long i = 0; i < count; i++) {
+                    a += 5;
+                }
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        thread.join();
+        System.out.println("concurrency: " + time + "ms,b=" + b);
+    }
+
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial: " + time + "ms,b=" + b + ",a=" + a);
+    }
+}
