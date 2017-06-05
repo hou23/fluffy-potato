33295a43f10aa76a3bb0e6d4168aa3e0ed3f0ad3
yaojie.hou <yaojie.hou@ilaw66.com>
2017-06-05 18:49:17 +0800

    线程应用实例

--- src/main/java/com/marshall/concurrent/base/Join.java
+++ src/main/java/com/marshall/concurrent/base/Join.java
@@ -1,12 +1,21 @@
 package com.marshall.concurrent.base;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by yaojie.hou on 2017/6/2.
  */
 public class Join {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws InterruptedException {
+		Thread previous = Thread.currentThread();
+		for (int i = 0; i < 10; i++) {
+			Thread thread = new Thread(new Domino(previous));
+			thread.start();
+			previous = thread;
+		}
+		TimeUnit.SECONDS.sleep(5);
+		System.out.println(Thread.currentThread().getName() + " terminate.");
 	}
 
 	static class Domino implements Runnable {
